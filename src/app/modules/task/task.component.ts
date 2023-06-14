9e51ac5e61d9bc0a5c8502623afa514c4543681d
Avellow <avellow@yandex.ru>
2023-06-14 16:41:35 +0300

    added template and styles to task-component

--- src/app/modules/task/task.component.ts
+++ src/app/modules/task/task.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { ITask } from 'src/app/services/task.service';
+import { ITask, TaskService } from 'src/app/services/task.service';
 
 @Component({
   selector: 'app-task',
@@ -10,5 +10,13 @@ export class TaskComponent {
   
   @Input() task!: ITask;
   @Output() remove = new EventEmitter<ITask>;
+
+  constructor(public taskService: TaskService) {}
+
+  handleCheck():void {
+    this.task.status == 'done' 
+      ? this.taskService.changeStatus(this.task, 'common')
+      : this.taskService.changeStatus(this.task, 'done')
+  }
   
 }
