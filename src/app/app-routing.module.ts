7f909674fcabf3657562906bf8ccfcb5fc14f973
Avellow <avellow@yandex.ru>
2023-06-19 18:13:17 +0300

    feat: added aboutPage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './pages/auth/login/login-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginPageComponent, pathMatch: 'full' },
+  { path: 'about', component: AboutPageComponent, pathMatch: 'full' }
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { ListModule } from './modules/list/list.module';
 import { AppRoutingModule } from './app-routing.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
 
 
 @NgModule({
