efe8c199bcd7c001f295379784b1cf67e26e64fd
Avellow <avellow@yandex.ru>
2023-06-20 18:12:13 +0300

    feat: added AuthGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,16 +4,17 @@ import { LoginPageComponent } from './pages/auth/login-page/login-page.component
 import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { LoginPageModule } from './pages/auth/login-page/login-page.module';
 import { ListComponent } from './modules/list/list.component';
+import { AuthGuard } from './shared/guards/auth.guard';
 
 const routes: Routes = [
   { path: 'login', component: LoginPageComponent },
   { path: 'about', component: AboutPageComponent },
-  { path: 'todo-list', component: ListComponent }
+  { path: 'todo-list', component: ListComponent, canActivate: [AuthGuard] }
 ]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes), LoginPageModule],
   exports: [RouterModule],
-  providers: [],
+  providers: [AuthGuard],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
